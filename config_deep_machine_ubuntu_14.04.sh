6b95eba46efd8d8a895baa74c15f8c5670ee9aec
Georges Petrov <georgesm.petrov@gmail.com>
2016-10-03 11:15:54 +0200

    Update config_deep_machine_ubuntu_14.04.sh

--- config_deep_machine_ubuntu_14.04.sh
+++ config_deep_machine_ubuntu_14.04.sh
@@ -2,6 +2,7 @@
 
 # Install Docker
 sudo su && \
+cd && \
 sudo apt-get update && \
 sudo apt-get install -y apt-transport-https ca-certificates && \
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \

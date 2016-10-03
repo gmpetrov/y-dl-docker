a4040551b8ad3bfa361e2bfb5a06c1774238430f
Georges Petrov <georgesm.petrov@gmail.com>
2016-10-03 10:45:00 +0200

    Update config_deep_machine_ubuntu_14.04.sh

--- config_deep_machine_ubuntu_14.04.sh
+++ config_deep_machine_ubuntu_14.04.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/shMAINTAINER Sai Soundararaj <saip@outlook.com>
+
 # Install Docker
 sudo su
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-echo “deb https://apt.dockerproject.org/repo ubuntu-trusty main” > /etc/apt/sources.list.d/docker.list
+echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main'> /etc/apt/sources.list.d/docker.list
 sudo apt-get update
 apt-cache policy docker-engine
 sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual

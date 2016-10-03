4c03b82c212d83424c7d4feddb8442ee3467cd02
Georges Petrov <georgesm.petrov@gmail.com>
2016-10-03 11:30:44 +0200

    Update config_deep_machine_ubuntu_14.04.sh

--- config_deep_machine_ubuntu_14.04.sh
+++ config_deep_machine_ubuntu_14.04.sh
@@ -1,8 +1,6 @@
 #!/bin/shnvidia-docker run -it -p 8888:8888 -p 6006:6006 -v /sharedfolder:/root/sharedfolder ysance/y-dl-docker:gpu bash
 
 # Install Docker
-sudo su && \
-cd && \
 sudo apt-get update && \
 sudo apt-get install -y apt-transport-https ca-certificates && \
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \

a2439db6a8aea6f8cc853385e75db0673bf8d519
Georges Petrov <georgesm.petrov@gmail.com>
2016-10-04 11:34:28 +0200

    Update config_deep_machine_ubuntu_14.04.sh

--- config_deep_machine_ubuntu_14.04.sh
+++ config_deep_machine_ubuntu_14.04.sh
@@ -2,8 +2,6 @@
 
 # Install Docker
 sudo apt-get update && \
-sudo apt-get upgrade && \
-sudo apt-get dist-upgrade && \
 sudo apt-get install -y apt-transport-https ca-certificates && \
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list && \
@@ -14,7 +12,22 @@ sudo apt-get update && \
 sudo apt-get install -y docker-engine && \
 sudo usermod -aG docker $USER && \
 
-# Blacklist nouveau drivers (conflict with Nvidia drivers)
+# Download and install Nvidia driver
+sudo apt-get install -y dkms linux-headers-generic && \
+wget http://us.download.nvidia.com/XFree86/Linux-x86_64/364.19/NVIDIA-Linux-x86_64-364.19.run && \
+sudo apt-get install -y gcc make && \
+chmod +x NVIDIA-* && \
+sudo ./NVIDIA-Linux-x86_64-364.19.run --dkms --silent && \
+modprobe nvidia && \
+
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb && \
+sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb && \
+
+# Download custom Dockerfile and build image
+wget https://raw.githubusercontent.com/gmpetrov/y-dl-docker/master/Dockerfile.gpu && \
+sudo docker build -t ysance/y-dl-docker:gpu -f Dockerfile.gpu . && \
+
+# Blacklist nouveau driver
 echo "blacklist nouveau" > /etc/modprobe.d/blacklist-nouveau.conf && \
 echo "blacklist lbm-nouveau" >> /etc/modprobe.d/blacklist-nouveau.conf && \
 echo "options nouveau modeset=0" >> /etc/modprobe.d/blacklist-nouveau.conf && \
@@ -23,21 +36,5 @@ echo "alias lbm-nouveau off" >> /etc/modprobe.d/blacklist-nouveau.conf && \
 echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf && \
 sudo update-initramfs -u && \
 
-sudo apt-get install -y dkms linux-headers-generic && \
-wget http://us.download.nvidia.com/XFree86/Linux-x86_64/364.19/NVIDIA-Linux-x86_64-364.19.run && \
-sudo apt-get install -y gcc make && \
-chmod +x NVIDIA-* && \
-sudo ./NVIDIA-Linux-x86_64-364.19.run --dkms --silent && \
-modprobe nvidia && \
-
-# Install nvidia-docker
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.0-rc.3/nvidia-docker_1.0.0.rc.3-1_amd64.deb && \
-sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb && \
-
-# Download custom Dockerfile and build image
-wget https://raw.githubusercontent.com/gmpetrov/y-dl-docker/master/Dockerfile.gpu && \
-sudo docker build -t ysance/y-dl-docker:gpu -f Dockerfile.gpu . && \
-
 # Run image
 nvidia-docker run -it -p 8888:8888 -p 6006:6006 -v /sharedfolder:/root/sharedfolder ysance/y-dl-docker:gpu bash
-
